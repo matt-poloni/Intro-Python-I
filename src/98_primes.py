16f71a5fd5cb97d64da1f94d161c81255e5dd81e
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-10-29 17:40:19 -0400

    Tweaks 98_primes to only run is_prime once

--- src/98_primes.py
+++ src/98_primes.py
@@ -2,6 +2,9 @@ from importlib import import_module
 is_prime = import_module("97_helpers").is_prime
 
 num = int(input("\nIS IT PRIME? "))
-yn = "YES" if is_prime(num) else "NO"
-nt = "n't" if not is_prime(num) else ""
+
+prime = is_prime(num)
+yn = "YES" if prime else "NO"
+nt = "n't" if not prime else ""
+
 print(f"{yn}, {num} is{nt} prime\n")
